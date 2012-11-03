9d333dc6bcd7e8844fee060e368bdf4e1961ce6d
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2012-11-03 17:19:25 +0900

    パラメータ渡し方変更

--- app/perl/update_cache_worker.pl
+++ app/perl/update_cache_worker.pl
@@ -10,7 +10,7 @@ my $path = shift || 1;
 
 my $ua = Furl->new;
 while (1) {
-    my $res = $ua->post("http://127.0.0.1:5001/ticket/update_table_cache/?process=$path");
+    my $res = $ua->post("http://127.0.0.1:5001/ticket/update_table_cache?process=$path");
     if ($res->is_success) {
     }
     else {
